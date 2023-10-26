14c5cd6ff98d67c25e221f15d52868b8cb602561
STUDENT oliver K. Maher <Oliver.K.Maher@students.ittralee.ie>
2023-10-26 10:52:06 +0100

    new ball things Signed-off-by: STUDENT oliver K. Maher <Oliver.K.Maher@students.ittralee.ie>

--- My first game/Assets/dalek_control.cs
+++ My first game/Assets/dalek_control.cs
@@ -5,28 +5,56 @@ using UnityEngine;
 
 public class dalek_control : MonoBehaviour
 {
+    cubeControl player;
     Animator dalekAnimator;
     enum dalekState { Idle, Attack, Follow}
     dalekState currentlyIs = dalekState.Idle;
+    private float aggroRadius = 10;
+    private float walkingSpeed = 0.3f;
+    private float meleeDistance = 1;
     // Start is called before the first frame update
     void Start()
     {
         dalekAnimator = GetComponent<Animator>();
-        Player = FindAnyObjectByType<Tardis>();
-        Player = 
+        player = FindAnyObjectByType<cubeControl>();
+        
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        switch (currentlyIs)
-                Case dalekState.Attack;
-            Break
-            if (Vector3.Distance(PlayerPrefs.transform.position, transform))
+        switch (currentlyIs) {
+          case dalekState.Idle:
+                if (Vector3.Distance(player.transform.position, transform.position) < aggroRadius)
+                {
+                    currentlyIs = dalekState.Follow;
+                    dalekAnimator.SetBool("isWalikng", true);
 
+                }
+                break;
 
+            case dalekState.Attack:
+               
+               
 
+                break;
+
+            case dalekState.Follow:
+                
+                    transform.LookAt(player.transform.position);
+                    transform.position += transform.forward * walkingSpeed * Time.deltaTime;
+
+                if (Vector3.Distance(player.transform.position, transform.position) < meleeDistance)
+
+                { currentlyIs = dalekState.Attack;
+                
+                }
+                break;
+
+                
+
+            }
         if (Input.GetKey(KeyCode.Space))
             dalekAnimator.SetBool("swimming", true);
         else
